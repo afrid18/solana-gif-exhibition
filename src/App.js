4fce57b436a89366d480530337cd77b5f831501b
afrid18 <afridhussain.d@gmail.com>
2022-05-30 23:50:52 +0530

    Trying to add Connect wallet

--- src/App.js
+++ src/App.js
@@ -7,79 +7,88 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  // Actions
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { solana } = window;
+    // Actions
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { solana } = window;
 
-      if (solana) {
-        if (solana.isPhantom) {
-          console.log('Phantom wallet found!');
-          const response = await solana.connect({ onlyIfTrusted: true });
-          console.log(
-            'Connected with Public Key:',
-            response.publicKey.toString()
-          );
+            if (solana) {
+                if (solana.isPhantom) {
+                    console.log('Phantom wallet found!');
+                    const response = await solana.connect({ onlyIfTrusted: true });
+                    console.log(
+                        'Connected with Public Key:',
+                        response.publicKey.toString()
+                    );
+                    setWalletAddress(response.publicKey.toString());
+                }
+            } else {
+                alert('Solana object not found! Get a Phantom Wallet 👻');
+            }
+        } catch (error) {
+            console.error(error);
         }
-      } else {
-        alert('Solana object not found! Get a Phantom Wallet 👻');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  /*
-   * Let's define this method so our code doesn't break.
-   * We will write the logic for this next!
-   */
-  const connectWallet = async () => {};
-
-  /*
-   * We want to render this UI when the user hasn't connected
-   * their wallet to our app yet.
-   */
-  const renderNotConnectedContainer = () => (
-    <button
-      className="cta-button connect-wallet-button"
-      onClick={connectWallet}
-    >
-      Connect to Wallet
-    </button>
-  );
-
-  // UseEffects
-  useEffect(() => {
-    const onLoad = async () => {
-      await checkIfWalletIsConnected();
     };
-    window.addEventListener('load', onLoad);
-    return () => window.removeEventListener('load', onLoad);
-  }, []);
 
-  return (
-    <div className="App">
-      <div className="container">
+    /*
+     * Let's define this method so our code doesn't break.
+     * We will write the logic for this next!
+     */
+    const connectWallet = async () => {
+        const { solana } = window;
+
+        if (solana) {
+            const response = await solana.connect();
+            console.log('Connected with Public Key:', response.publicKey.toString());
+            setWalletAddress(response.publicKey.toString());
+        }
+    };
+
+    /*
+     * We want to render this UI when the user hasn't connected
+     * their wallet to our app yet.
+     */
+    const renderNotConnectedContainer = () => (
+        <button
+        className="cta-button connect-wallet-button"
+        onClick={connectWallet}
+        >
+        Connect to Wallet
+        </button>
+    );
+
+    // UseEffects
+    useEffect(() => {
+        const onLoad = async () => {
+            await checkIfWalletIsConnected();
+        };
+        window.addEventListener('load', onLoad);
+        return () => window.removeEventListener('load', onLoad);
+    }, []);
+
+    return (
+        <div className="App">
+        <div className="container">
         <div className="header-container">
-          <p className="header">Afrid Hussain</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
-          {/* Render your connect to wallet button right here */}
-          {renderNotConnectedContainer()}
+        <p className="header">Afrid Hussain</p>
+        <p className="sub-text">
+        View your GIF collection in the metaverse ✨
+        </p>
+        {/* Render your connect to wallet button right here */}
+        {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+        <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <a
+        className="footer-text"
+        href={TWITTER_LINK}
+        target="_blank"
+        rel="noreferrer"
+        >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+        </div>
+    );
 };
 
 export default App;
